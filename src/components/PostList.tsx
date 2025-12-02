650dff02da729088a6c85cec6d49781e8bf60d3f
jayvilla <villarina.jeffrey@gmail.com>
2025-12-01 17:56:07 -0800

    add PostList component

---
+++ src/components/PostList.tsx
@@ -0,0 +1,19 @@
+import PostCard from "./PostCard";
+
+interface Post {
+  id: string;
+  content: string;
+  author: string;
+  createdAt: string;
+  likes: number;
+}
+
+export default function PostList({ posts }: { posts: Post[] }) {
+  return (
+    <div className="space-y-4">
+      {posts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
+    </div>
+  );
+}
